8496e5817f374b360d29bdb3126cd9858e48afa4
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-02-10 19:05:35 -0400

    initial commit

---
+++ src/main/java/dev/amerida/algoritms/App.java
@@ -0,0 +1,13 @@
+package dev.amerida.algoritms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
