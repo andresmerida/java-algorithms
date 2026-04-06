64874fdab16a459707dfc40be1dbc13ba3af4d75
Andres Merida <andresmerida1@gmail.com>
2026-04-05 23:43:58 -0400

    example with scanner

---
+++ src/main/java/dev/amerida/algoritms/scanner/ScannerTest.java
@@ -0,0 +1,16 @@
+package dev.amerida.algoritms.scanner;
+
+import java.util.Scanner;
+
+/**
+ * Desarrolle un programa que permita cargar numeros enteros no repetidos a un arreglo de tamaño 10.
+ * A continuacion debera determinar el maximo y minimo de los numeros del arreglo.
+ * Tambien indicar cuantos valores no se encuentran presentes, e indicar la suma de los numeros del arreglo.
+ */
+public class ScannerTest {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
